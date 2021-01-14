506af97679c9ca0e8a94d26511a923b2c7b24818
Denis <kdmlwf@gmail.com>
2021-01-14 16:18:06 +0200

    images

---
+++ configs/configs.php
@@ -0,0 +1,7 @@
+<?php
+
+// токен бота
+define('TELEGRAM_TOKEN', '1490991801:AAGBvkuD6HL46G4jggdH4aMCP0ejqQMEf80');
+  
+// внутренний айди
+define('TELEGRAM_CHATID', '684931136');
---
+++ configs/settings.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Файл для настроек сайта
+*/
+
+$user_c = null;
+
+if(isset($_COOKIE["user_id"])) {
+    $user_c = $_COOKIE["user_id"];
+}
+?>
